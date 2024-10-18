547c6a0fdaf1de17052b8e27ce8d4ac670387b40
Nobuo Yoshie <n.yoshie.tp@exv.co.jp>
2024-10-18 14:23:00 +0900

    testセキュリティ

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ console.log("Hello, Worldsssっっっｓ!400011122222sssddkalkjldddddddddddetest
 console.log("secret:***REMOVED***");
 console.log("secret:***REMOVED***");
 console.log("f3");
+console.log("aws_access_key_id:123498765432222");
